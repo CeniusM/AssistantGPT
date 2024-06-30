9e812aaf8def5ae34b21f1398ab06151bbcd8e26
CeniusM <93879193+CeniusM@users.noreply.github.com>
2024-07-01 01:03:26 +0200

    working on syntax guidelines (work in progress)

---
+++ syntaxGuideLines.py
@@ -0,0 +1,9 @@
+class some_class:
+    def __init__(self):
+        self.a = 1
+
+    def some_funtion(msg: str) -> str:
+        return msg + "did something"
+
+if __name__ == "__main__":
+    print("debugging some_class like a pro")
