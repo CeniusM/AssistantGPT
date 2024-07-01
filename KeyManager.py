f27cd071cb912eb373ac407ecfd1ce15ba7bbc97
CeniusM <93879193+CeniusM@users.noreply.github.com>
2024-07-01 13:55:40 +0200

    fixed keys file path

--- KeyManager.py
+++ KeyManager.py
@@ -2,7 +2,7 @@ from FileManager import *
 import json
 
 def load_keys():
-    file = read_text_file("resources\\sKey")
+    file = read_text_file("Keys\\sKey")
     return json.loads(file)
 
 def get_GPT_key():
