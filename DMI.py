6e3126d191bd8311eac9f30d4762808538e13ac4
Samuel Oslo <samuel.oslo.rasmussen@gmail.com>
2024-07-01 13:22:15 +0200

    Dmi

---
+++ DMI.py
@@ -0,0 +1,18 @@
+
+class DMI:
+    
+
+    def get_location():
+        pass
+    
+    def api_call():
+        pass
+
+    def create_response(location, api_response):
+        pass
+    
+    def get_weather():
+        pass
+
+
+    
