3e3d59b964071c0a4b61e05e5a269a1fe8362616
CeniusM <93879193+CeniusM@users.noreply.github.com>
2024-07-01 13:53:24 +0200

    fixed key manager, key names

--- KeyManager.py
+++ KeyManager.py
@@ -6,10 +6,10 @@ def load_keys():
     return json.loads(file)
 
 def get_GPT_key():
-    return load_keys()["GPT_Key"]
+    return load_keys()["GPT_KEY"]
 
 def get_EL_key():
-    return load_keys()["EL_Key"]
+    return load_keys()["EL_KEY"]
 
 def load_audio():
     pass
