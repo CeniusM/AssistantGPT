f58ec130be1bc4dcdb42428927233f7c62bd1c33
CeniusM <93879193+CeniusM@users.noreply.github.com>
2024-07-01 14:45:23 +0200

    Added text and json write funcs

--- FileManager.py
+++ FileManager.py
@@ -11,5 +11,16 @@ def read_text_file(path):
     return ""
 
 def read_json_file(path):
-    file = read_text_file(path)
-    return json.loads(file)
+    content = read_text_file(path)
+    return json.loads(content)
+
+def write_text_file(path, content):
+    with open(path, "w") as file:
+        file.write(content)
+
+def write_json_file(path, content):
+    try:
+        with open(path, "w") as file:
+            json.dump(content, file, indent=4)
+    except IOError:
+        print(f"Error: An I/O error occurred while writing JSON to the file at {path}.")
