148c9085a1a1c533d94016a97c087328fcb9b075
Samuel Oslo <samuel.oslo.rasmussen@gmail.com>
2024-07-07 11:47:47 +0200

    WebSearch.create_qeury works now

--- WebSearch.py
+++ WebSearch.py
@@ -5,20 +5,24 @@ class WebSearch:
         
     def create_response(user_input="search the web to figure out the age of dolphins"):
         #create parameters and make api call
-        search_prompt = WebSearch.create_search_prompt(user_input=user_input)
+        search_query = WebSearch.create_search_query(user_input=user_input)
         dependencies = WebSearch.create_dependencies()
         api_data = WebSearch.api_call(dependencies)
 
-        #convert the data and get the weather info
-        converted_data = WebSearch.convert_weather_units(api_data)
-        weather_list = WebSearch.get_weather_info(converted_data, api_data)
-        filtered_weather_info = WebSearch.filter_weather_info(weather_list)
+        #convert the data from the search
+        converted_data = WebSearch.convert_api_data(api_data)
+        search_info = WebSearch.manage_data(converted_data)
         
-        altered_user_input = user_input+"\n All the weather data available, if needed:"+filtered_weather_info
+        altered_user_input = user_input+"\n Search info, if needed: "+search_info
         return altered_user_input 
 
 
-    def create_search_prompt(user_input):
+    def create_search_query(user_input):
         #create the wanted search info using chatGPT
-        search_prompt_convo = ConversationManager(promptname="search_prompt.txt").api_convo_setup(user_input)
-        weather_info = ChatGPT.prompt(search_prompt_convo, silent=True)
+        search_query_convo = ConversationManager(promptname="search_query.txt").api_convo_setup(user_input)
+        search_query = ChatGPT.prompt(search_query_convo, silent=True)
+        return search_query
+
+
+if __name__ == "__main__":
+    print(WebSearch.create_search_query("search the web to figure out the age of dolphins"))
