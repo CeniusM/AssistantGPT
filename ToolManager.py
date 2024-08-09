e2134940170d0b7685917716051388b16bd2523a
CeniusM <93879193+CeniusM@users.noreply.github.com>
2024-08-09 13:22:40 +0200

    Adding code for easier tool managment

--- ToolManager.py
+++ ToolManager.py
@@ -1,9 +1,35 @@
+from varname import nameof
 from DMI import *
 from Memory import *
 from WebSearch import *
 from SmartMic import *
+from Tooling.Tools import *
 
-#this document has been created to solve import error
+# Returns a dict of the avalible tools. It include the names, tool description and function to be called
+def get_available_tools():
+    tools = {
+        nameof(get_weather): {
+            "description": get_weather_forecast_description(),
+            "func": get_weather
+        },
+        
+        nameof(look_through_memory): {
+            "description": look_through_memory_description(),
+            "func": look_through_memory
+        },
+        
+        nameof(web_search): {
+            "description": web_search_description(),
+            "func": web_search
+        },
+        
+        nameof(adjust_mic): {
+            "description": adjust_microphone_description(),
+            "func": adjust_mic
+        }
+    }
+    
+    return tools
 
 def get_weather(GPT_parameters):
     return DMI.create_response(GPT_parameters)
