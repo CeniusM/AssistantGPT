6805251faa6219e3a2c289bc9f9e122c0af6d5c0
CeniusM <93879193+CeniusM@users.noreply.github.com>
2024-08-14 19:03:09 +0200

    Working on new Agent based class

--- Agent_GPT.py
+++ Agent_GPT.py
@@ -2,9 +2,16 @@
 
 import openai
 import json
-from enum import Enum
 
 from FileUtil import *
+
+class ToolInterface:
+    def __init__(self):
+        self.name = ""
+    def call(self, args):
+        pass
+    def __dict__(self):
+        pass
 
 class Role:
     SYS = "system"
@@ -16,108 +23,88 @@ class AgentAction:
     AUTO = "auto"
     TEXT = "none"
 
-class Prompt:
+class Agent_GPT:
     def __init__(self,
-            sys_msg: str = "",
+            key,
+            sys_msg: str = None,
             messages: list = [],
             agent_action: AgentAction = AgentAction.AUTO,
-            tools: list = None,
+            tools: list[ToolInterface] = None,
             temperature: float = 0.5
         ) -> None:
         
+        self.key = key
         self.sys_msg = sys_msg
         self.messages = messages
         self.agent_action = agent_action
         self.tools = tools
         self.temperature = temperature
-    
+        
     def message(self, role: Role, content: str):
         self.messages.append({"role": role, "content": content})
 
-class PromptResponse:
-    def __init__(self) -> None:
-        self.response = ""
-        self.tools_called = []
-
-class ChatGPT:
-    def prompt_agent(key, prompt: Prompt) -> PromptResponse:
+    def prompt_agent(self):
         # Setup
-        openai.api_key = key
-        result = PromptResponse()
+        openai.api_key = self.key
 
         # Setup system message
-        messages = prompt.messages.copy()
-        if prompt.sys_msg:
-            messages.insert(-1, {"role": Role.SYS, "content": prompt.sys_msg})
+        messages = self.messages
+        if self.sys_msg:
+            messages.insert(-1, {"role": Role.SYS, "content": self.sys_msg})
         
         # API call
         completion = openai.chat.completions.create(
             model = "gpt-3.5-turbo-0125",
             messages = messages,
-            temperature = prompt.temperature,
+            temperature = self.temperature,
 
             # Unpack if tools are used
             **{
-                "tools": [tool.__dict__() for tool in prompt.tools],
-                "tool_choice": prompt.agent_action
-            } if prompt.agent_action is not AgentAction.TEXT else {}
+                "tools": [tool.__dict__() for tool in self.tools],
+                "tool_choice": self.agent_action
+            } if self.agent_action is not AgentAction.TEXT else {}
         )
 
         # Get api response
         message = completion.choices[0].message
         response = message.content
         tool_calls = message.tool_calls
-        
-        result.response = response
 
         # Return if no tools called
         if AgentAction.TEXT:
-            return result
+            return (response, None)
+        
+        tools_called = []
         
         # Parse tool calls
         for tool_call in tool_calls:
             tool_name = tool_call.function.name
             tool_args = json.loads(tool_call.function.arguments)
-            tool = [t for t in prompt.tools if t.name == tool_name]
+            tool = [t for t in self.tools if t.name == tool_name]
 
             if len(tool) != 1:
                 raise Exception("Tool not found")
             
             tool[0].call(tool_args)
 
-            result.tools_called.append(tool_name)
+            tools_called.append(tool_name)
         
-        return result
+        return (response, tools_called)
         
 
 
 
 
 if __name__ == "__main__":
-    prompt = Prompt(
+    agent = Agent_GPT(
         sys_msg="Only write code",
         agent_action=AgentAction.TEXT
     )
     
-    prompt.message(Role.USER, "Can you make a small python program that counts from 1 to 10")
+    agent.message(Role.USER, "Can you make a small python program that counts from 1 to 10")
 
-    result = ChatGPT.prompt_agent(File.read_text("Keys\sKey"), prompt)
+    result = agent.prompt_agent()
     
     print(result.response)
     print(result.tools_called)
 
-
-
-
-
-
-
-
-
-# class ToolInterface:
-#     def __init__(self):
-#         self.name = ""
-#     def call(self, args):
-#         pass
-#     def __dict__(self):
-#         pass
