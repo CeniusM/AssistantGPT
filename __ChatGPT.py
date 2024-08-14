f36b74a1aa9a30a553821fb809b66f4251241e89
CeniusM <93879193+CeniusM@users.noreply.github.com>
2024-08-14 18:50:22 +0200

    New idea for ChatGPT

--- __ChatGPT.py
+++ __ChatGPT.py
@@ -1,27 +1,35 @@
 # A new possible way of implementing the ChatGPT prompting
 
 import openai
+import json
 from enum import Enum
 
-class Role(Enum):
-    sys = "system"
-    user = "user"
-    agent = "assistant"
+from FileUtil import *
 
-class AgentAction(Enum):
-    tool = "required"
-    auto = "auto"
-    text = "none"
+class Role:
+    SYS = "system"
+    USER = "user"
+    AGENT = "assistant"
+
+class AgentAction:
+    TOOL = "required"
+    AUTO = "auto"
+    TEXT = "none"
 
 class Prompt:
-    def __init__(self) -> None:
-        self.sys_msg: str = ""
-        self.messages: list = []
-        self.silent: bool = False
-        self.temperature: float = 0.5
-        self.ignore_response: bool = False
-        self.tools: list = None
-        self.agent_action: AgentAction = AgentAction.auto
+    def __init__(self,
+            sys_msg: str = "",
+            messages: list = [],
+            agent_action: AgentAction = AgentAction.AUTO,
+            tools: list = None,
+            temperature: float = 0.5
+        ) -> None:
+        
+        self.sys_msg = sys_msg
+        self.messages = messages
+        self.agent_action = agent_action
+        self.tools = tools
+        self.temperature = temperature
     
     def message(self, role: Role, content: str):
         self.messages.append({"role": role, "content": content})
@@ -30,17 +38,19 @@ class PromptResponse:
     def __init__(self) -> None:
         self.response = ""
         self.tools_called = []
-        self.action = None
 
 class ChatGPT:
     def prompt_agent(key, prompt: Prompt) -> PromptResponse:
+        # Setup
         openai.api_key = key
+        result = PromptResponse()
 
+        # Setup system message
         messages = prompt.messages.copy()
-
         if prompt.sys_msg:
-            messages.insert(-1, prompt.sys_msg)
+            messages.insert(-1, {"role": Role.SYS, "content": prompt.sys_msg})
         
+        # API call
         completion = openai.chat.completions.create(
             model = "gpt-3.5-turbo-0125",
             messages = messages,
@@ -50,19 +60,51 @@ class ChatGPT:
             **{
                 "tools": [tool.__dict__() for tool in prompt.tools],
                 "tool_choice": prompt.agent_action
-            } if prompt.agent_action is not AgentAction.text else {}
+            } if prompt.agent_action is not AgentAction.TEXT else {}
         )
 
-        response = completion.choices[0].message
-        text = response.response
-        tool_calls = response.tool_calls
+        # Get api response
+        message = completion.choices[0].message
+        response = message.content
+        tool_calls = message.tool_calls
+        
+        result.response = response
 
+        # Return if no tools called
+        if AgentAction.TEXT:
+            return result
+        
+        # Parse tool calls
+        for tool_call in tool_calls:
+            tool_name = tool_call.function.name
+            tool_args = json.loads(tool_call.function.arguments)
+            tool = [t for t in prompt.tools if t.name == tool_name]
+
+            if len(tool) != 1:
+                raise Exception("Tool not found")
+            
+            tool[0].call(tool_args)
+
+            result.tools_called.append(tool_name)
+        
+        return result
         
 
 
 
 
+if __name__ == "__main__":
+    prompt = Prompt(
+        sys_msg="Only write code",
+        agent_action=AgentAction.TEXT
+    )
+    
+    prompt.message(Role.USER, "Can you make a small python program that counts from 1 to 10")
 
+    result = ChatGPT.prompt_agent(File.read_text("Keys\sKey"), prompt)
+    
+    print(result.response)
+    print(result.tools_called)
 
 
 
