7d73ba1d28ba1c0de1b0c9769dc74cfd8c84b437
CeniusM <93879193+CeniusM@users.noreply.github.com>
2024-08-14 13:54:29 +0200

    Added comments to entry for clarity

--- entry.py
+++ entry.py
@@ -8,8 +8,15 @@ from ConversationTools import *
 from TextChecker import *
 from Graphics.GUI import *
 
+# Set up GUI
 gui, thread = GUI.start_async_window(800, 600)
+gui.add_user(user="user", alias="You", rgb=(250, 250, 250))
+gui.add_user(user="assistant", alias="GPT", rgb=(50, 200, 150))
+Listening = (40, 70, 40)
+Thinking = (30, 50, 80)
+Responding = (40, 40, 40)
 
+# Set up conversations
 conversation_manager = set_global_conversation_manager()
 conversation_manager.convo_setup()
 mic = SmartMic()
@@ -17,23 +24,27 @@ SmartSpeaker.beep()
 
 try:
     while gui.running:
-
-        gui.clear_color = (40, 80, 80)
+        # Get user input
+        gui.clear_color = Listening
         text = mic.listen_and_interpret()
-        gui.clear_color = (100, 60, 60)
 
+        # Parse user input
         if check_text_for_exit(text): break
         
         conversation_formatted = conversation_manager.add_and_get("user", text)
-        gui.message("You", text)
+        gui.message("user", text)
 
+        # Generate GPT response
+        gui.clear_color = Thinking
         response = ChatGPT.smart_prompt(conversation_formatted)
-        gui.clear_color = (40, 40, 40)
 
-        gui.message("GPT", response)
+        # Play response
+        gui.clear_color = Responding
+        gui.message("assistant", response)
         conversation_manager.add_paragraph("assistant", response)
         SmartSpeaker.play_voice(response)
 
+        # Backup conversation
         conversation_manager.save()
 
 except Exception as e:
